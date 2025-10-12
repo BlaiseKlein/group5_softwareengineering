5c87ab8c4df607e79a26ab89a3d90f85f1ff0cca
Joe Lin <jlin249@my.bcit.ca>
2025-10-12 05:09:34 -0700

    Added image storage to docker volume and image serving api for image upload, and modified translation result to use uploaded photo. Added media config constants to settings

--- backend/image_handling/views.py
+++ backend/image_handling/views.py
@@ -1,15 +1,51 @@
 import os
+import uuid
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework import status
+from django.conf import settings
+from django.http import FileResponse
 
 class ImageTranslate(APIView):
+
     def post(self, request):
+
         uploaded_file = request.FILES.get("file")
-        if uploaded_file:
-            return Response({
-                "filename": uploaded_file.name,
-                "translation": "Banana"
-            }, status=status.HTTP_200_OK)
-        return Response({"error": "No file uploaded"}, status=status.HTTP_400_BAD_REQUEST)
+        if not uploaded_file:
+            return Response({"error":"No file uploaded"}, status=status.HTTP_400_BAD_REQUEST)
+        elif not uploaded_file.content_type.startswith("image/"):
+                return Response({"error:Uploaded file is not an image"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        print("Received files:", uploaded_file.name)
+        print("Content type:", uploaded_file.content_type)
+        print("Size:", uploaded_file.size, "bytes")
+
+        # Get folder path
+        save_folder = os.path.join(settings.MEDIA_ROOT, "images")
+        os.makedirs(save_folder, exist_ok=True)
+
+        # Save picture to volume
+        filename = f"{uuid.uuid4().hex}_{uploaded_file.name}"
+        save_path = os.path.join(save_folder, filename)
+        with open(save_path, 'wb+') as destination:
+            for chunk in uploaded_file.chunks():
+                 destination.write(chunk)
+
+        db_path = os.path.join("images", filename)
+        frontend_path = request.build_absolute_uri(settings.MEDIA_URL + db_path)
+
+        return Response({
+             "filename": filename,
+             "db_path": db_path,
+             "url": frontend_path,
+             "translation": "Banana"
+        }, status=status.HTTP_200_OK)
+    
+
+class ServeImage(APIView):
+    def get(self, request, filename):
+        file_path = os.path.join(settings.MEDIA_ROOT, "images", filename)
+        if os.path.exists(file_path):
+            return FileResponse(open(file_path, "rb"))
+        return Response({"error": "File not found"}, status=status.HTTP_404_NOT_FOUND)
