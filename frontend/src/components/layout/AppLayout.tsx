ec1f865e42cabc8e2bd48b4ddf9d17f7b6e682cf
Inez Yoon <inez3cs@gmail.com>
2025-11-13 17:33:28 -0800

    Clean layout component

--- frontend/src/components/layout/AppLayout.tsx
+++ frontend/src/components/layout/AppLayout.tsx
@@ -1,31 +1,19 @@
 /**
- * Basic layout for bottom navigation
- * 
- * TODO:       
- * Add hide_nav 
+ * Layout for navigation buttons.
+ * This layout is located at the bottom of the screen. (Mobile friendly)
+ * main.tsx handles the routes with(and without) this bottom navigatin layout
  */
-import { Outlet, useLocation } from "react-router-dom";
+
+import { Outlet } from "react-router-dom";
 import BottomRadialNav from "../../components/nav/BottomNav.tsx";
 
-// Nav should not work when
-// 1. camera is working
-// 2. ...
-const HIDE_NAV = new Set([
-  "/login",
-  "/register",
-  "/translation/processing",
-]);
-
 export default function AppLayout() {
-  const { pathname } = useLocation();
-  const hideNav = HIDE_NAV.has(pathname);
-
   return (
     <div className="min-h-screen bg-white">
       <div className="mx-auto w-full max-w-[420px] px-5 pb-[120px]">
         <Outlet />
       </div>
-      {!hideNav && <BottomRadialNav />}
+      {<BottomRadialNav />}
     </div>
   );
 }
